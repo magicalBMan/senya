b2403e769a3ce36561bfee2c9468dc73ad70713b
David Sayers <47674066+magicalBMan@users.noreply.github.com>
2021-11-04 21:22:47 -0600

    Added AppBarConfig

--- app/src/main/java/com/sayers/senya/ui/MainActivity.kt
+++ app/src/main/java/com/sayers/senya/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.sayers.senya.R
 import com.sayers.senya.data.Attraction
 import com.sayers.senya.data.AttractionsResponse
@@ -14,6 +17,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     val attractionsList: List<Attraction> by lazy {
         parseAttractions()
@@ -26,6 +30,13 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun parseAttractions(): List<Attraction> {
